07e19f4eb216a2d58fff4ae58fc20b9d06cb78ed
unknown <ivancaroromero31@gmail.com>
2023-10-09 09:32:18 +0200

    ejercicio 1

--- Ejercicio 1/src/App.java
+++ Ejercicio 1/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el primer número: ");
+        int a = sc.nextInt();
+        System.out.println("Introduzca el segundo número: ");
+        int b = sc.nextInt();
+        System.out.println("El producto de "+a+" y "+b+" es: "+(a*b));
     }
 }
